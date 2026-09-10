2d97deaa10e1eb0d4fe3986445196e8703619fab
fdsbrian <brianfds@gmail.com>
2026-09-10 12:05:14 +0300

    Configure Astro for GitHub

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || '';
-const isProjectPage = process.env.GITHUB_ACTIONS === 'true' && repo && !repo.endsWith('.github.io');
-export default defineConfig({ site: process.env.SITE_URL || 'https://example.github.io', base: isProjectPage ? `/${repo}` : '/', integrations:[sitemap()] });
+
+export default defineConfig({
+  site: 'https://fdsbrian.github.io',
+  base: '/brian-portfolio',
+  integrations: [sitemap()],
+});
